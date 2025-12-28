39f58f020dcc649daa51fbf507b2d29093f1e921
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2025-12-28 16:46:48 +0700

    Remove RoleGuard from Layout component

--- app/(homepage)/layout.js
+++ app/(homepage)/layout.js
@@ -3,9 +3,9 @@ import RoleGuard from "@/components/shared/RoleGuard";
 
 export default function Layout({ children }) {
     return (
-        <RoleGuard allowedRoles={["PESERTA"]}>
+        <>
             <Navbar />
             {children}
-        </RoleGuard>
+        </>
     );
 }
